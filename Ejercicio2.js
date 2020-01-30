5c3c0d77c7d765b96872fc1b60d9eaa7b28ebee8
amayandonaegui <60475597+amayandonaegui@users.noreply.github.com>
2020-01-30 09:35:36 -0600

    Update Ejercicio2.js

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,1 +1,11 @@
-
+escribo esto  mientras desayuno
+que puedo desayunar 
+huevito con chorizo 
+yogurt con granola y miel
+pan tostado y crema de cacahuate
+café y rebanada de pastel
+avena con plátano
+papaya 
+quesadillas
+chilaquiles
+tacos de frijoles
